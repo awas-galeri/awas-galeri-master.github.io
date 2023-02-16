7b64cc92fe26e849735eb90636cf5b63b6907962
Awas Galeri <awasgaleri@gmail.com>
2023-02-16 15:21:28 +0700

    first commit master website awa-galeri

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Parallax
+// About
+// $(window).on('load', function () {
+//     $('.pKiri').addClass('pMuncul');
+//     $('.pKanan').addClass('pMuncul');
+// })
+
+
+// Parallax
+$(window).scroll(function () {
+
+    // Jumbotron
+    let wScroll = $(this).scrollTop();
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 9 + '%)'
+    });
+
+    // About
+    if (wScroll > $('#about').offset().top - 150) {
+        $('.pKiri').addClass('pMuncul');
+        $('.pKanan').addClass('pMuncul');
+    }
+
+
+    // Portfolio
+    if (wScroll > $('#portfolio').offset().top - 250) {
+        $('#portfolio .card').each(function (i) {
+            setTimeout(function () {
+                $('#portfolio .card').eq(i).addClass('muncul');
+            }, 400 * (i + 1));
+        });
+    }
+});
